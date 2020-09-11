b7d00d3c02752ca8c93d03866dcab2ceace5ffc0
JUNGHOJONG <hojong1351@naver.com>
2020-09-11 23:40:11 +0900

    프로그래머스 - 비밀지도(리펙토링)

--- Kakao_2018_01_secretMap.java
+++ Kakao_2018_01_secretMap.java
@@ -1,10 +1,19 @@
 package algorithm;
 
+import java.util.Arrays;
+
 public class Kakao_2018_01_secretMap {
     public static void main(String[] args) {
         int n = 6;
         int[] arr1 = { 46, 33, 33 ,22, 31, 50 };
         int[] arr2 = { 27 ,56, 19, 14, 14, 10 };
+        Solution_secretMap processSolution = new Solution_secretMap();
+        System.out.println(Arrays.toString(processSolution.solution(n, arr1, arr2)));
+    }
+}
+
+class Solution_secretMap{
+    public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] output = new String[n];
         for( int i=0; i<n; i++ ){
             output[i] = Integer.toBinaryString( arr1[i] | arr2[i] );
@@ -14,6 +23,6 @@ public class Kakao_2018_01_secretMap {
                 output[i] = " " + output[i];
             }
         }
-        System.out.println( output );
+        return output;
     }
 }
